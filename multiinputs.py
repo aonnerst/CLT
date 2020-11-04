6c3e375484ad9dc7f4d8ce34fc4ea2e411eb3c79
aonnerst <56915766+aonnerst@users.noreply.github.com>
2020-11-04 14:40:34 +0200

    multiinput but single output

--- multiinputs.py
+++ multiinputs.py
@@ -34,5 +34,55 @@ for i in range(0,len(pdfnames)):
 fin = [ROOT.TFile(elm) for elm in Infiles];
 #fin = [ROOT.TFile(elm) for elm in InfilesTest];
 
-for i in range(0,len(pdfnames)):
-	fin[i].Print();
+i = 1;
+
+
+fin[i].Print();
+# add labels for each pad
+plables = ["Central Limit Theorem"];
+# model names : for histonames in ROOT file
+
+# Following two must be added
+toptitle = "Central Limit Theorem"; # need to add on the top
+dataDetail = ["more"];
+
+
+plot = JPyPlotRatio.JPyPlotRatio(panels=(1,1),
+	disableRatio=[],    #to get rid of the 0th ratio panel
+	axisLabelSize=9,
+	tickLabelSize=7,
+	#rowBounds=ylimits,  # for nrow
+	#colBounds=xlimits,  # for ncol
+	panelLabel=plables,  # nrowxncol
+	#ratioBounds=rlimits,# for nrow
+#	ratioSystPlot=True,
+
+	panelLabelLoc=(0.55,0.57),panelLabelSize=5,panelLabelAlign="left",
+	legendPanel=0,
+	legendLoc=(0.73,0.80),legendSize=5,xlabel="sample mean",ylabel="# averages");
+
+
+	#plot.EnableLatex(True); # for publication need fonts via texlive
+
+#load histogram from fin
+gr = fin[i].Get("CentralLimit");
+fit = fin[i].Get("gaus");
+
+
+#add histogram
+p1 = plot.Add(0, gr, color="orchid", linestyle="dotted", label="sampled");
+p2 = plot.Add(0, fit, color="red",label="fit");
+plot.Ratio(p1,p2)
+
+
+
+fin[i].Close();
+
+plot.Plot();
+
+	
+figures = "figs/plot_{:s}.pdf".format(pdfnames[i]);
+print("{:s}".format(figures));
+plot.Save(figures)
+
+
